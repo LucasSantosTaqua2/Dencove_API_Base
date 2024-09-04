92d9d1d6bca472c226a9430702c6e013b16d448a
Lucas Santos <lucassantosdev@hotmail.com>
2024-09-04 08:17:06 -0300

    Initial

---
+++ Dencove_API/Controllers/UsuarioController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Dencove_API.Data;
+using Dencove_API.Models;
+using Dencove_API.Helper;
+
+namespace Dencove_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public UsuarioController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuario
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UsuarioModel>>> GetUsuarioModels()
+        {
+            return await _context.UsuarioModels.ToListAsync();
+        }
+
+        // GET: api/Usuario/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> GetUsuarioModel(Guid id)
+        {
+            var usuarioModel = await _context.UsuarioModels.FindAsync(id);
+
+            if (usuarioModel == null)
+            {
+                return NotFound();
+            }
+
+            return usuarioModel;
+        }
+
+        // PUT: api/Usuario/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuarioModel(Guid id, UsuarioModel usuarioModel)
+        {
+            if (id != usuarioModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuarioModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Usuario
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> PostUsuarioModel(UsuarioModel usuarioModel, string senha)
+        {
+           /* usuarioModel.Senha = senha;
+            senha = senha.GerarHash(); */
+
+            _context.UsuarioModels.Add(usuarioModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuarioModel", new { id = usuarioModel.Id }, usuarioModel);
+        }
+
+        // DELETE: api/Usuario/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuarioModel(Guid id)
+        {
+            var usuarioModel = await _context.UsuarioModels.FindAsync(id);
+            if (usuarioModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.UsuarioModels.Remove(usuarioModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsuarioModelExists(Guid id)
+        {
+            return _context.UsuarioModels.Any(e => e.Id == id);
+        }
+    }
+}
